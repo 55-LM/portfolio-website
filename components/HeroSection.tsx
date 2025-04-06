1f26c3fcaca8e2be27cd7f8a3e355eecc60f0a7b
Alvi <64937667+55-LM@users.noreply.github.com>
2025-04-06 19:52:15 -0400

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,38 +1,55 @@
-"use client" // this is a client component
-import React from "react"
-import Image from "next/image"
-import { Link } from "react-scroll/modules"
-import { HiArrowDown } from "react-icons/hi"
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { Link } from "react-scroll/modules";
+import { HiArrowDown } from "react-icons/hi";
 
 const HeroSection = () => {
   return (
-    <section id="home" className="relative w-full h-screen flex items-center justify-center">
-      <div className="absolute inset-0 -z-10">
+    <section
+      id="home"
+      className="relative w-full h-screen flex items-center justify-center"
+    >
+      {/* Background image */}
+      <div className="absolute inset-0 -z-10 flex items-center justify-center">
         <Image
-          src="/gradientcirclebg.png"
+          src="/Image 20.png" // Light-mode version only
           alt=""
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"
-          className="opacity-60"
+          width={1000}
+          height={1000}
+          className="opacity-60 blur-md -translate-y-12 transition-all duration-500"
         />
       </div>
+
       {/* Content */}
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 px-6 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl text-neutral-800 dark:text-white">
-            Hi, I&#39;m Alvi!
+          <h1
+            style={{ fontFamily: "EditorialNew", fontSize: "3rem" }}
+            className="text-neutral-800"
+          >
+            Alvi Alam
           </h1>
-          <p className="text-lg mt-4 mb-6 md:text-2xl text-neutral-800 dark:text-gray-300">
-            I&#39;m a{" "}
-            <span className="font-semibold">
-              Computer Engineering{" "}
-            </span>
-            student at Toronto Metropolitan University who loves exploring the connection between software and hardware, and figuring out cool ways to turn ideas into reality
+
+          <p
+            style={{ fontFamily: "NeueMontreal", fontSize: "1.25rem" }}
+            className="mt-4 text-neutral-800"
+          >
+            Student at Toronto Metropolitan University studying{" "}
+            <span
+              style={{ fontFamily: "EditorialNew", fontStyle: "italic" }}
+            >
+              Computer Engineering
+            </span>{" "}
+            , exploring at the intersection of software and hardware to bring
+            ideas to life
           </p>
+
           <Link
             to="projects"
-            className="font-semibold px-6 py-3 bg-black text-white dark:bg-white dark:text-black rounded shadow hover:opacity-90"
+            style={{ fontFamily: "EditorialNew", fontStyle: "italic" }}
+            className="inline-block mt-6 text-xl text-neutral-800 cursor-pointer transition-all after:content-[''] after:block after:h-[2px] after:bg-current after:transition-all after:duration-300 after:scale-x-0 hover:after:scale-x-100 after:origin-left"
             activeClass="active"
             spy={true}
             smooth={true}
@@ -43,6 +60,8 @@ const HeroSection = () => {
           </Link>
         </div>
       </div>
+
+      {/* Arrow */}
       <div className="flex flex-row items-center text-center justify-center absolute bottom-10">
         <Link
           to="about"
@@ -54,12 +73,12 @@ const HeroSection = () => {
         >
           <HiArrowDown
             size={35}
-            className="animate-bounce text-neutral-800 dark:text-white"
+            className="animate-bounce text-neutral-800"
           />
         </Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
