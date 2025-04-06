5667a5b38f936d717573489ffe532c96cdfb7576
Alvi <64937667+55-LM@users.noreply.github.com>
2025-04-06 19:51:50 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,39 +1,22 @@
-import React from "react"
-import {
-  AiOutlineGithub,
-  AiOutlineLinkedin,
-} from "react-icons/ai"
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
-      <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
-      <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
-        <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
-          © 2025 Alvi Alam<a href="/" className="hover:underline"></a>
-        </div>
-        <div className="flex flex-row items-center justify-center space-x-2 mb-1">
-          <a href="https://github.com/55-LM" rel="noreferrer" target="_blank">
-            <AiOutlineGithub
-              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
-              size={30}
-            />
-          </a>
-
-          <a
-            href="https://www.linkedin.com/in/alvia22"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <AiOutlineLinkedin
-              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
-              size={30}
-            />
-          </a>
-        </div>
+    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+      <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0" />
+      <div className="mx-auto p-4 flex flex-col md:flex-row justify-between items-center text-neutral-500">
+        <span
+          style={{ fontFamily: "EditorialNew" }}
+          className="text-neutral-800"
+        >
+          Alvi Alam
+        </span>
+        <span>© 2025</span>
       </div>
     </footer>
-  )
-}
+  );
+};
+
+export default Footer;
 
 export default Footer
