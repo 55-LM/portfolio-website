69c705b7fd67cb6ffa3522fe0af160819ff97360
Alvi <64937667+55-LM@users.noreply.github.com>
2025-04-06 19:50:30 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,25 @@
-"use client"
-import "../styles/globals.css"
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
-import { ThemeProvider } from "next-themes"
+"use client";
+import "../styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body className="dark:bg-stone-900">
-        <ThemeProvider enableSystem={true} attribute="class">
-          <Navbar />
-          {children}
-          <Footer />
-        </ThemeProvider>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Portfolio</title>
+      </head>
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
